141278c9bcc80b2ed97b812ab0aaef74b149f414
Katja <katja@klahr.se>
2023-10-02 15:23:00 +0200

    Förkalrande text på knapapr inlagd.

--- app/src/main/java/com/katja/groupcalkylator/MainActivity.java
+++ app/src/main/java/com/katja/groupcalkylator/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 // Ibrahim, Katja, Goran, Alex, Dennis
-// Ja, visst är vi bara 5 i gruppen och inte 6?
 
 public class MainActivity extends AppCompatActivity {
 
