2f753dc457f343a44ebf2789cc3b24455d147b2a
Katja <katja@klahr.se>
2023-10-02 13:51:36 +0200

    Ny kommentar

--- app/src/main/java/com/katja/groupcalkylator/MainActivity.java
+++ app/src/main/java/com/katja/groupcalkylator/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 // Ibrahim, Katja, Goran, Alex, Dennis
+// Ja, visst är vi bara 5 i gruppen och inte 6?
 
 public class MainActivity extends AppCompatActivity {
 
