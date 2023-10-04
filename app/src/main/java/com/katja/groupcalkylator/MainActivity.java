cc2128dabc99bfe2078f74d8f7ad0d5d70dda0e6
Goran <damjanic@hotmail.com>
2023-10-04 09:51:40 +0200

    Deklarerat id till EditText, Button och TextView i MainActivity.

--- app/src/main/java/com/katja/groupcalkylator/MainActivity.java
+++ app/src/main/java/com/katja/groupcalkylator/MainActivity.java
@@ -3,8 +3,23 @@ package com.katja.groupcalkylator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.groupcalkylator.R;
+
+// Ibrahim, Katja, Goran, Alex, Dennis
+// Ja, visst är vi bara 5 i gruppen och inte 6?
+//TestGoran
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText0, editText1;
+    Button buttonPlus, buttonMinus, buttonMultiplication,
+            buttonDivision, buttonRoot, buttonPercent, buttonPhytagoras, buttonCircleArea,
+            buttonCylinderVolume;
+    TextView textAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,3 +27,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
