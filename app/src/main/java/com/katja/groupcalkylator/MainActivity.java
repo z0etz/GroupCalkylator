3b96f61d78d83b64fa9e12e06366d67c7ddd3628
Goran <damjanic@hotmail.com>
2023-10-04 10:09:27 +0200

    Deklarerat findViewById till EditText, Button och TextView

--- app/src/main/java/com/katja/groupcalkylator/MainActivity.java
+++ app/src/main/java/com/katja/groupcalkylator/MainActivity.java
@@ -25,6 +25,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText0 = findViewById(R.id.editText0);
+        editText1 = findViewById(R.id.editText1);
+        buttonPlus = findViewById(R.id.buttonPlus);
+        buttonMinus = findViewById(R.id.buttonMinus);
+        buttonMultiplication = findViewById(R.id.buttonMultiplication);
+        buttonDivision = findViewById(R.id.buttonDivision);
+        buttonRoot = findViewById(R.id.buttonRoot);
+        buttonPercent = findViewById(R.id.buttonPercent);
+        buttonPhytagoras = findViewById(R.id.buttonPhytagoras);
+        buttonCircleArea = findViewById(R.id.buttonCircleArea);
+        buttonCylinderVolume = findViewById(R.id.buttonCylinderVolume);
+        textAnswer = findViewById(R.id.textAnswer);
     }
 }
 
